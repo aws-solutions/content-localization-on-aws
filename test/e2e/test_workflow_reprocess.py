3fd6092fbf399cb12731186c8e96d128155f44ed
aburkleaux-amazon <33331299+aburkleaux-amazon@users.noreply.github.com>
2021-08-03 10:16:08 -0700

    run in headless mode in automated envs

--- test/e2e/test_workflow_reprocess.py
+++ test/e2e/test_workflow_reprocess.py
@@ -14,9 +14,9 @@ def browser():
     chrome_options = Options()
     
     ####### TESTING - remove headless to see browser actions
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--start-maximized")
     ####### TESTING - remove headless to see browser actions
     
     browser = webdriver.Chrome(chrome_options=chrome_options)

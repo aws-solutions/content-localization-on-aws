da3765ea84c3b67b74787c6f7b6ed331c6d14863
ianwow <ianwow@amazon.com>
2021-08-25 12:15:11 -0700

    rename template url variables to clarify what they represent

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -389,10 +389,10 @@ if [ "$global_bucket" != "solutions-reference" ] && [ "$global_bucket" != "solut
   echo "Without existing MIE deployment:"
   echo "TEMPLATE='"https://"$global_bucket"."$s3domain"/aws-content-localization/"$version"/aws-content-localization.template"'"
 
-  touch templateUrlMieDevelopment.txt
-  echo "https://"$global_bucket"."$s3domain"/aws-content-localization/"$version"/aws-content-localization.template" > templateUrlMieDevelopment.txt
-  touch templateUrlMieRelease.txt
-  echo "https://"$global_bucket"."$s3domain"/aws-content-localization/"$version"/aws-content-localization-use-existing-mie-stack.template" > templateUrlMieRelease.txt
+  touch template_url_that_uses_an_existing_mie_stack.txt
+  echo "https://"$global_bucket"."$s3domain"/aws-content-localization/"$version"/aws-content-localization.template" > template_url_that_uses_an_existing_mie_stack.txt
+  touch template_url_that_deploys_mie_solution_as_nested_stack.txt template_url_that_deploys_mie_solution_as_nested_stack.txt
+  echo "https://"$global_bucket"."$s3domain"/aws-content-localization/"$version"/aws-content-localization-use-existing-mie-stack.template" > template_url_that_deploys_mie_solution_as_nested_stack.txt
 
 fi
 

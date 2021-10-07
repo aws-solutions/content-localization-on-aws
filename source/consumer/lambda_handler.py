3813e6ec0b5d7a6f2a8e5cdc8611aa68fef0b500
aburkleaux-amazon <33331299+aburkleaux-amazon@users.noreply.github.com>
2021-10-06 17:53:55 -0700

    Workflow and Operator keys are set search data

--- source/consumer/lambda_handler.py
+++ source/consumer/lambda_handler.py
@@ -642,9 +642,11 @@ def process_webcaptions(asset, workflow, results, language_code):
     metadata_type = "webcaptions"+"_"+language_code
 
     webcaptions = metadata
-    webcaptions["workflow"] = workflow
+    webcaptions["Workflow"] = workflow
+    webcaptions["Operator"] = metadata_type
     es = connect_es(es_endpoint)
     index_document(es, asset, metadata_type, webcaptions)
+
 
 def process_transcribe(asset, workflow, results, type):
     metadata = json.loads(results)
@@ -673,7 +675,8 @@ def process_transcribe(asset, workflow, results, type):
 
         item["confidence"] = confidence
         item["content"] = content
-        item["workflow"] = workflow
+        item["Workflow"] = workflow
+        item["Operator"] = "transcribe"
 
         transcribe_items.append(item)
 

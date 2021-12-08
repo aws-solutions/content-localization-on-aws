d8d98210fb6020ebdea6b8f10f35e2c1110f9922
ianwow <ianwow@amazon.com>
2021-12-08 11:14:35 -0800

    replace CAS workflow with CL workflow

--- doc/sigv4_post_sample.py
+++ doc/sigv4_post_sample.py
@@ -1,12 +1,12 @@
 ###############################################################################
 #
-# PURPOSE: This code sample shows how to start the CasImageWorkflow using a
-#   sigv4 signed request to the workflow execution REST API in the Media
+# PURPOSE: This code sample shows how to start the ContentLocalizationWorkflow 
+#   using a sigv4 signed request to the workflow execution REST API in the Media
 #   Insights Engine.
 #
 ###############################################################################
 #
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # This file is licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License. A copy of the
@@ -24,25 +24,8 @@
 # This version makes a POST request and passes request parameters
 # in the body (payload) of the request. Auth information is passed in
 # an Authorization header.
-import sys, os, base64, datetime, hashlib, hmac
-import requests # pip install requests
-
-# ************* REQUEST VALUES *************
-method = 'POST'
-service = 'execute-api'
-# Host is the base URL for your REST API, where {restapi_id} is the API identifier, {region} is the Region of the API deployment.
-host = '{restapi_id}.execute-api.{region}.amazonaws.com'
-region = '{region}'
-# Endpoint is the endpoint URL for your REST API resource, where {api_name} is the API name, and {method_name} is the name of the method resource of the API deployment.
-endpoint = 'https://{restapi_id}.execute-api.us-west-2.amazonaws.com/api/{api_name}/{method_name}'
-# POST requests use a content type header.
-content_type = 'application/json'
-
-# Specify the Amazon S3 location for the input media file:
-s3_bucket = '{s3_bucket}'
-s3_key = '{s3_key}'
-# Request parameters for executing the CasImageWorkflow in its default configuration
-request_parameters =  '{"Name":"CasImageWorkflow", "Input":{"Media":{"Image":{"S3Bucket": "' + s3_bucket + '", "S3Key":"' + s3_key + '"}}}}'
+import sys, os, datetime, hashlib, hmac
+import requests
 
 # Key derivation functions. See:
 # http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
@@ -56,90 +39,108 @@ def getSignatureKey(key, date_stamp, regionName, serviceName):
     kSigning = sign(kService, 'aws4_request')
     return kSigning
 
-# Read AWS access key from env. variables or configuration file. Best practice is NOT
-# to embed credentials in code.
-access_key = os.environ.get('AWS_ACCESS_KEY_ID')
-secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-if access_key is None or secret_key is None:
-    print('No access key is available.')
-    sys.exit()
+def lambda_handler(event, context):
+    print("Received event:", event)
+    # ************* REQUEST VALUES *************
+    method = 'POST'
+    service = 'execute-api'
+    # Host is the base URL for your REST API, where {restapi_id} is the API identifier, {region} is the Region of the API deployment. 
+    host = '{restapi_id}.execute-api.{region}.amazonaws.com'
+    region = '{region}'
+    # Endpoint is the endpoint URL for your REST API resource, where {api_name} is the API name, and {method_name} is the name of the method resource of the API deployment. 
+    endpoint = 'https://{restapi_id}.execute-api.us-west-2.amazonaws.com/api/{api_name}/{method_name}'
+    # POST requests use a content type header.
+    content_type = 'application/json'
 
-# Create a date for headers and the credential string
-t = datetime.datetime.utcnow()
-amz_date = t.strftime('%Y%m%dT%H%M%SZ')
-date_stamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+    # Specify the Amazon S3 location for the input media file:
+    s3_bucket = '{s3_bucket}'
+    s3_key = event["Records"][0]["s3"]["object"]["key"]
+    # Request parameters for executing the CasImageWorkflow in its default configuration
+    request_parameters = '{"Name":"ContentLocalizationWorkflow","Configuration":{"PreprocessVideo":{"Thumbnail":{"ThumbnailPosition":"2","Enabled":true},"Mediainfo":{"Enabled":true}},"AnalyzeVideo":{"faceDetection":{"Enabled":false},"technicalCueDetection":{"Enabled":false},"shotDetection":{"Enabled":false},"celebrityRecognition":{"MediaType":"Video","Enabled":false},"labelDetection":{"MediaType":"Video","Enabled":true},"personTracking":{"MediaType":"Video","Enabled":false},"faceSearch":{"MediaType":"Video","Enabled":false,"CollectionId":"undefined"},"textDetection":{"MediaType":"Video","Enabled":false},"Mediaconvert":{"MediaType":"Video","Enabled":false},"TranscribeVideo":{"Enabled":true,"TranscribeLanguage":"en-US","MediaType":"Audio"}},"TransformText":{"WebToSRTCaptions":{"MediaType":"MetadataOnly","TargetLanguageCodes":["es","de","en"],"Enabled":true},"WebToVTTCaptions":{"MediaType":"MetadataOnly","TargetLanguageCodes":["es","de","en"],"Enabled":true},"PollyWebCaptions":{"MediaType":"MetadataOnly","Enabled":false,"SourceLanguageCode":"en"}},"WebCaptions":{"WebCaptions":{"MediaType":"MetadataOnly","SourceLanguageCode":"en","Enabled":true}},"Translate":{"Translate":{"MediaType":"Text","Enabled":false},"TranslateWebCaptions":{"MediaType":"MetadataOnly","Enabled":true,"TargetLanguageCodes":["es","de"],"SourceLanguageCode":"en","TerminologyNames":[],"ParallelDataNames":[]}},"AnalyzeText":{"ComprehendEntities":{"MediaType":"Text","Enabled":false},"ComprehendKeyPhrases":{"MediaType":"Text","Enabled":false}}}, "Input":{"Media":{"Video":{"S3Bucket": "' + s3_bucket + '", "S3Key":"' + s3_key + '"}}}}'
+
+    # Read AWS access key from env. variables or configuration file. Best practice is NOT
+    # to embed credentials in code.
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    if access_key is None or secret_key is None:
+        print('No access key is available.')
+        sys.exit()
+
+    # Create a date for headers and the credential string
+    t = datetime.datetime.utcnow()
+    amz_date = t.strftime('%Y%m%dT%H%M%SZ')
+    # Date w/o time, used in credential scope:
+    date_stamp = t.strftime('%Y%m%d') 
+
+    # ************* TASK 1: CREATE A CANONICAL REQUEST *************
+    # http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+
+    # Step 1 is to define the verb (GET, POST, etc.)--already done.
+
+    # Step 2: Create canonical URI--the part of the URI from domain to query 
+    # string (use '/' if no path)
+    canonical_uri = '/api/workflow/execution'
+
+    ## Step 3: Create the canonical query string. In this example, request
+    # parameters are passed in the body of the request and the query string
+    # is blank.
+    canonical_querystring = ''
+
+    # Step 4: Create the canonical headers. Header names must be trimmed
+    # and lowercase, and sorted in code point order from low to high.
+    # Note that there is a trailing \n.
+    canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n'
+
+    # Step 5: Create the list of signed headers. This lists the headers
+    # in the canonical_headers list, delimited with ";" and in alpha order.
+    # Note: The request can include any headers; canonical_headers and
+    # signed_headers include those that you want to be included in the
+    # hash of the request. "Host" and "x-amz-date" are always required.
+    signed_headers = 'host;x-amz-date'
+
+    # Step 6: Create payload hash. In this example, the payload (body of
+    # the request) contains the request parameters.
+    payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
+
+    # Step 7: Combine elements to create canonical request
+    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+    # ************* TASK 2: CREATE THE STRING TO SIGN*************
+    # Match the algorithm to the hashing algorithm you use, either SHA-1 or
+    # SHA-256 (recommended)
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
+    string_to_sign = algorithm + '\n' + amz_date + '\n' + credential_scope + '\n' + hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
+
+    # ************* TASK 3: CALCULATE THE SIGNATURE *************
+    # Create the signing key using the function defined above.
+    signing_key = getSignatureKey(secret_key, date_stamp, region, service)
+
+    # Sign the string_to_sign using the signing_key
+    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
 
 
-# ************* TASK 1: CREATE A CANONICAL REQUEST *************
-# http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+    # ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
+    # Put the signature information in a header named Authorization.
+    authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
 
-# Step 1 is to define the verb (GET, POST, etc.)--already done.
+    # For API Gateway, the request can include any headers, but MUST include "x-amz-date",
+    # "x-amz-target", "content-type", and "Authorization". Except for the authorization
+    # header, the headers must be included in the canonical_headers and signed_headers values, as
+    # noted earlier. Order here is not significant.
+    # Python note: The 'host' header is added automatically by the Python 'requests' library.
+    headers = {'Authorization': authorization_header,
+               'Content-Type': content_type,
+               'x-amz-date': amz_date}
 
-# Step 2: Create canonical URI--the part of the URI from domain to query
-# string (use '/' if no path)
-canonical_uri = '/api/workflow/execution'
+    # ************* SEND THE REQUEST *************
+    print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
+    print('Request URL = ' + endpoint)
+    print('request_parameters = ' + str(request_parameters))
+    print('headers = ' + str(headers))
 
-## Step 3: Create the canonical query string. In this example, request
-# parameters are passed in the body of the request and the query string
-# is blank.
-canonical_querystring = ''
+    r = requests.post(endpoint, data=request_parameters, headers=headers)
 
-# Step 4: Create the canonical headers. Header names must be trimmed
-# and lowercase, and sorted in code point order from low to high.
-# Note that there is a trailing \n.
-canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n'
-
-# Step 5: Create the list of signed headers. This lists the headers
-# in the canonical_headers list, delimited with ";" and in alpha order.
-# Note: The request can include any headers; canonical_headers and
-# signed_headers include those that you want to be included in the
-# hash of the request. "Host" and "x-amz-date" are always required.
-signed_headers = 'host;x-amz-date'
-
-# Step 6: Create payload hash. In this example, the payload (body of
-# the request) contains the request parameters.
-payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
-
-# Step 7: Combine elements to create canonical request
-canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
-
-
-# ************* TASK 2: CREATE THE STRING TO SIGN*************
-# Match the algorithm to the hashing algorithm you use, either SHA-1 or
-# SHA-256 (recommended)
-algorithm = 'AWS4-HMAC-SHA256'
-credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
-string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
-
-# ************* TASK 3: CALCULATE THE SIGNATURE *************
-# Create the signing key using the function defined above.
-signing_key = getSignatureKey(secret_key, date_stamp, region, service)
-
-# Sign the string_to_sign using the signing_key
-signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-
-
-# ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
-# Put the signature information in a header named Authorization.
-authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-
-# For API Gateway, the request can include any headers, but MUST include "x-amz-date",
-# "x-amz-target", "content-type", and "Authorization". Except for the authorization
-# header, the headers must be included in the canonical_headers and signed_headers values, as
-# noted earlier. Order here is not significant.
-# Python note: The 'host' header is added automatically by the Python 'requests' library.
-headers = {'Authorization': authorization_header,
-           'Content-Type': content_type,
-           'x-amz-date': amz_date}
-
-# ************* SEND THE REQUEST *************
-print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
-print('Request URL = ' + endpoint)
-print('request_parameters = ' + str(request_parameters))
-print('headers = ' + str(headers))
-
-r = requests.post(endpoint, data=request_parameters, headers=headers)
-
-print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
-print('Response code: %d\n' % r.status_code)
-print(r.text)
+    print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
+    print('Response code: %d\n' % r.status_code)
+    print(r.text)
